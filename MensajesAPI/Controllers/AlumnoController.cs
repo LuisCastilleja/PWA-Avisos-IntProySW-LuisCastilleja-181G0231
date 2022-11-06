c4aff19095817d66971c5e9adfd4f35c5140684f
Luis Castilleja <luis.castilleja@r2devpros.com>
2022-11-06 15:50:38 -0600

    API (doing) pendiente POST y PUT

--- MensajesAPI/Controllers/AlumnoController.cs
+++ MensajesAPI/Controllers/AlumnoController.cs
@@ -14,8 +14,16 @@ namespace MensajesAPI.Controllers
 
         Repository<AlumnoMensaje> repositoryAlumno_Mensaje;
         Repository<Mensaje> repositoryMensajes;
+        Repository<EspecialidadMensaje> repositoryEspecialidad_Mensaje;
+        Repository<GrupoMensaje> repositoryGrupo_Mensaje;
+        Repository<Alumno> repositoryAlumno;
 
         List<Mensaje> listaMensajes = new List<Mensaje>();
+        List<AlumnoMensaje> listaAlumnos_Mensajes = new List<AlumnoMensaje>();
+        List<Mensaje> listaTablaMensajes = new List<Mensaje>();
+        List<EspecialidadMensaje> listaEspecialidad_Mensajes = new List<EspecialidadMensaje>();
+        List<GrupoMensaje> listaGrupo_Mensajes = new List<GrupoMensaje>();
+
 
         public AlumnoController(Proyecto_mensajes_pwa_bdContext context, IConfiguration configuration)
         {
@@ -23,23 +31,63 @@ namespace MensajesAPI.Controllers
             Configuration = configuration;
             repositoryMensajes = new Repository<Mensaje>(Context);
             repositoryAlumno_Mensaje = new Repository<AlumnoMensaje>(Context);
+            repositoryEspecialidad_Mensaje = new Repository<EspecialidadMensaje>(Context);
+            repositoryGrupo_Mensaje = new Repository<GrupoMensaje>(Context);
+            repositoryAlumno = new Repository<Alumno>(Context);
         }
         //Obtener los mensajes que ha enviado el profesor.
         [HttpGet("{id}")]
         public IEnumerable<Mensaje> Get(int id)
         {
-            var listaAlumnos_Mensajes = repositoryAlumno_Mensaje.GetAll().Where(x => x.FkIdAlumno == id).ToList();
-            var listaTablaMensajes = repositoryMensajes.GetAll().ToList();
-            foreach (var alumno_mensaje in listaAlumnos_Mensajes)
+            var alumno = repositoryAlumno.GetById(id);
+            if (alumno != null)
             {
-                foreach (var mensaje in listaTablaMensajes)
+                listaTablaMensajes = repositoryMensajes.GetAll().ToList();
+                listaAlumnos_Mensajes = repositoryAlumno_Mensaje.GetAll().Where(x => x.FkIdAlumno == alumno.Id).ToList();
+                listaEspecialidad_Mensajes = repositoryEspecialidad_Mensaje.GetAll().Where(x => x.FkIdEspecialidad == alumno.FkIdEspecialidad).ToList();
+                listaGrupo_Mensajes = repositoryGrupo_Mensaje.GetAll().Where(x => x.FkIdGrupo == alumno.FkIdGrupo).ToList();
+            }     
+            //Traer los mensajes que se le enviaron al alumno individualmente
+            if (listaAlumnos_Mensajes != null && listaTablaMensajes!=null)
+            {
+                foreach (var alumno_mensaje in listaAlumnos_Mensajes)
                 {
-                    if (alumno_mensaje.FkIdMensaje == mensaje.Id)
+                    foreach (var mensaje in listaTablaMensajes)
                     {
-                        listaMensajes.Add(mensaje);
+                        if (alumno_mensaje.FkIdMensaje == mensaje.Id)
+                        {
+                            listaMensajes.Add(mensaje);
+                        }
                     }
                 }
-               
+            }
+            //Traer los mensajes que se le enviaron al alumno por grupo
+            if (listaGrupo_Mensajes != null && listaTablaMensajes != null)
+            {
+                foreach (var grupo_mensaje in listaGrupo_Mensajes)
+                {
+                    foreach (var mensaje in listaTablaMensajes)
+                    {
+                        if (grupo_mensaje.FkIdMensaje == mensaje.Id)
+                        {
+                            listaMensajes.Add(mensaje);
+                        }
+                    }
+                }
+            }
+            //Traer los mesajes que se le enviaron al alumno por especialidad
+            if (listaEspecialidad_Mensajes != null && listaTablaMensajes != null)
+            {
+                foreach (var especialidad_mensaje in listaEspecialidad_Mensajes)
+                {
+                    foreach (var mensaje in listaTablaMensajes)
+                    {
+                        if (especialidad_mensaje.FkIdMensaje == mensaje.Id)
+                        {
+                            listaMensajes.Add(mensaje);
+                        }
+                    }
+                }
             }
             return listaMensajes;
         }
--- MensajesAPI/Controllers/DocenteController.cs
+++ MensajesAPI/Controllers/DocenteController.cs
@@ -13,18 +13,116 @@ namespace MensajesAPI.Controllers
         public IConfiguration Configuration { get; }
 
         Repository<Mensaje> repositoryMensaje;
+        Repository<Especialidad> repositoryEspecialidad;
+        Repository<Grupo> repositoryGrupo;
+        Repository<Docente> repositoryDocente;
+        Repository<DocenteEspecialidad> repositoryDocente_Especialidad;
+        Repository<DocenteGrupo> repositoryDocente_Grupo;
+        List<DocenteGrupo> listaDocente_Grupo = new List<DocenteGrupo>();
+        List<DocenteEspecialidad> listaDocente_Especialidad = new List<DocenteEspecialidad>();
+        List<Especialidad> listaTablaEspecialidad = new List<Especialidad>();
+        List<Grupo> listaTablaGrupos = new List<Grupo>();
+        List<Especialidad> listaEspecialidades = new List<Especialidad>();
+        List<Grupo> listaGrupos = new List<Grupo>();
 
         public DocenteController(Proyecto_mensajes_pwa_bdContext context, IConfiguration configuration)
         {
             Context = context;
             Configuration = configuration;
             repositoryMensaje = new Repository<Mensaje>(Context);
+            repositoryEspecialidad = new Repository<Especialidad>(Context);
+            repositoryGrupo = new Repository<Grupo>(Context);
+            repositoryDocente = new Repository<Docente>(Context);
+            repositoryDocente_Especialidad = new Repository<DocenteEspecialidad>(Context);
+            repositoryDocente_Grupo = new Repository<DocenteGrupo>(Context);
         }
         //Obtener los mensajes que ha enviado el profesor.
         [HttpGet("{id}")]
         public IEnumerable<Mensaje> Get(int id)
         {
             return repositoryMensaje.GetAll().Where(x => x.FkIdDocente == id);
+        }
+
+        [HttpGet("Especialidades/{id}")]
+        public IEnumerable<Especialidad> GetEspecialidades(int id)
+        {
+            Docente docente = repositoryDocente.GetById(id);
+            if (docente != null)
+            {
+                listaDocente_Especialidad = repositoryDocente_Especialidad.GetAll().Where(x => x.FkIdDocente == docente.Id).ToList();
+                listaTablaEspecialidad = repositoryEspecialidad.GetAll().ToList();
+            }
+            if (listaDocente_Especialidad != null && listaTablaEspecialidad != null)
+            {
+                foreach (var docente_Especialidad in listaDocente_Especialidad)
+                {
+                    foreach (var especialidad in listaTablaEspecialidad)
+                    {
+                        if (docente_Especialidad.FkIdEspecialidad == especialidad.Id)
+                        {
+                            listaEspecialidades.Add(especialidad);
+                        }
+                    }
+                }
+            }
+            return listaEspecialidades;
+        }
+        
+        
+        [HttpGet("Grupos/{id}")]
+        public IEnumerable<Grupo> GetGrupos(int id)
+        {
+            Docente docente = repositoryDocente.GetById(id);
+            if (docente != null)
+            {
+                listaDocente_Grupo = repositoryDocente_Grupo.GetAll().Where(x => x.FkIdDocente == docente.Id).ToList();
+                listaTablaGrupos = repositoryGrupo.GetAll().ToList();
+            }
+            if (listaDocente_Grupo != null && listaTablaGrupos != null)
+            {
+                foreach (var docente_grupo in listaDocente_Grupo)
+                {
+                    foreach (var grupo in listaTablaGrupos)
+                    {
+                        if (docente_grupo.FkIdGrupo == grupo.Id)
+                        {
+                            listaGrupos.Add(grupo);
+                        }
+                    }
+                }
+            }
+            return listaGrupos;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Mensaje mensaje)
+        {
+            if(mensaje == null)
+            {
+                return BadRequest();
+            }
+            if(string.IsNullOrWhiteSpace(mensaje.Mensaje1))
+            {
+                ModelState.AddModelError("", "Proporcione el mensaje a enviar");
+            }
+            if (string.IsNullOrWhiteSpace(mensaje.Asunto))
+            {
+                ModelState.AddModelError("", "Proporcione el asunto a enviar");
+            }
+            if (string.IsNullOrWhiteSpace(mensaje.Destinatarios))
+            {
+                ModelState.AddModelError("", "Proporcione los destinatarios del mensaje");
+            }
+            if (ModelState.IsValid)
+            {
+                //Insertar a todas las tablas correspondientes
+                //Extraer los destinatarios en una lista y luego recorrerla para separar por grupo, especialidad y alumno.
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
         }
     }
 }
